cd32fe16e9a1955c6edb34b4da9490c74978915c
Gustavo Mendes <gustamms@hotmail.com>
2021-05-27 22:11:19 -0300

    Adicionado Push notifications com firebase

--- lib/Home.dart
+++ lib/Home.dart
@@ -1,6 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:esc/Login.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -8,6 +10,44 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  final FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
+  String _message = "";
+
+  _marcarPonto() async{
+    Firestore db = Firestore.instance;
+    FirebaseAuth auth = FirebaseAuth.instance;
+    FirebaseUser user = await auth.currentUser();
+
+    db.collection("pontos")
+    .add({"user": user.uid, "dateTime": DateTime.now()});
+    setState(() => _message = "Ponto adicionado");
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _firebaseMessaging.configure(
+      onMessage: (Map<String, dynamic> message) async {
+        print("onMessage: $message");
+        setState(() => _message = message["notification"]["body"]);
+      },
+      onLaunch: (Map<String, dynamic> message) async {
+        print("onLaunch: $message");
+        setState(() => _message = message["notification"]["body"]);
+      },
+      onResume: (Map<String, dynamic> message) async {
+        print("onResume: $message");
+        setState(() => _message = message["notification"]["body"]);
+      },
+    );
+
+    _firebaseMessaging.getToken().then((String token) {
+      assert(token != null);
+      print('Push token gerado: $token');
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,7 +71,53 @@ class _HomeState extends State<Home> {
             },
           )
         ],
-      )
+      ),
+        body: Container(
+            decoration: BoxDecoration(color: Color(0xff0)),
+            padding: EdgeInsets.all(16),
+            child: Center(
+              child: SingleChildScrollView(
+
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: <Widget>[
+
+                      Padding(
+                        padding: EdgeInsets.only(bottom: 120),
+                        child: Center(
+                          child: Text(
+                            "Registro de ponto",
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 20
+                            ),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(top: 16, bottom: 10),
+                        child: RaisedButton(
+                            child: Text(
+                              "Marcar",
+                              style: TextStyle(color: Colors.white, fontSize: 30),
+                            ),
+                            color: Colors.green,
+                            shape: CircleBorder(),
+                            padding: EdgeInsets.all(100.0),
+                            onPressed: (){
+                                _marcarPonto();
+                            }
+                        ),
+                      ),
+                      Container(
+                          child: Center(
+                            child: Text(_message),
+                          )),
+                    ]
+                ),
+              ),
+            )
+        )
     );
   }
 }
